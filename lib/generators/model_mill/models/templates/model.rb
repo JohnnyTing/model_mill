d76ebf20f8753e3010303791dcb644d56e313fb5
JohnnyTing <JohnnyTing2DingXu@gmail.com>
2019-10-27 16:19:41 +0800

    🐛 fix a bug, due to the old version of rails

--- lib/generators/model_mill/models/templates/model.rb
+++ lib/generators/model_mill/models/templates/model.rb
@@ -1,5 +1,4 @@
 class <%= options[:namespace] ? options[:namespace].camelize + '::' : '' %><%= @model_name %> < <%= options[:namespace] ? options[:namespace].camelize : 'ActiveRecord' %>::Base
-  attr_accessible <%= @columns.map(&:name).sort.map{|c| ":#{c}"}.join(', ') %>
 
 <% @associations.each do |cardinality, associations|
   -%><% associations.each do |a|
